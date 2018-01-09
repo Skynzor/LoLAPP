35d40ca237cdb145586a9147e3f6c4c7f1f046ea
Rene Cordes <renecordes@hotmail.com>
2018-01-09 13:14:48 +0100

    ReneLOCAL

--- app/src/main/java/skynzor/lolapp/MainScreen.java
+++ app/src/main/java/skynzor/lolapp/MainScreen.java
@@ -9,5 +9,13 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        /**
+         *
+         * Hier moet dan de button komen met een onclick event die in activity_main_screen.xml staat
+         *
+         */
+
+
     }
 }
