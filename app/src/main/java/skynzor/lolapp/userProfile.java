f0721c7effea83f134a55b0fd3d293a27e54be35
Rene Cordes <renecordes@hotmail.com>
2018-01-16 21:21:20 +0100

    Google plus Sign in fixed. Return states added. Memorize added. Cleaning sign out cache added.

--- app/src/main/java/skynzor/lolapp/userProfile.java
+++ app/src/main/java/skynzor/lolapp/userProfile.java
@@ -1,5 +1,6 @@
 package skynzor.lolapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -16,8 +17,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
@@ -25,12 +26,24 @@ public class userProfile extends AppCompatActivity  implements View.OnClickListe
 
     public LinearLayout prof_section;
     public Button SignOut;
-    public SignInButton SignIn;
+    public Button goHome;
     public TextView name,email;
     public ImageView prof_pic;
     public GoogleApiClient googleApiClient;
     public static final int REQ_CODE = 9001;
 
+
+    public void goHome() {
+        goHome = findViewById(R.id.btn_back);
+
+        goHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goHome = new Intent(userProfile.this, MainScreen.class);
+
+                startActivity(goHome);
+            }
+        });}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +51,6 @@ public class userProfile extends AppCompatActivity  implements View.OnClickListe
         setContentView(R.layout.activity_user_profile);
         prof_section = (LinearLayout)findViewById(R.id.prof_section);
         SignOut = (Button)findViewById(R.id.btn_logout);
-        SignIn = (SignInButton) findViewById(R.id.btn_login);
-        SignIn.setOnClickListener(this);
         SignOut.setOnClickListener(this);
         name = (TextView)findViewById(R.id.name);
         email = (TextView)findViewById(R.id.email);
@@ -48,14 +59,13 @@ public class userProfile extends AppCompatActivity  implements View.OnClickListe
 
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
+
+        goHome();
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_login:
-                signIn();
-                break;
             case R.id.btn_logout:
                 signOut();
                 break;
@@ -78,10 +88,10 @@ public class userProfile extends AppCompatActivity  implements View.OnClickListe
         }
     }
 
-    public void signIn(){
+    /*public void signIn(){
         Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
         startActivityForResult(intent,REQ_CODE);
-    }
+    }*/
 
     public void handleResult(GoogleSignInResult result){
 
@@ -99,24 +109,65 @@ public class userProfile extends AppCompatActivity  implements View.OnClickListe
         }
     }
 
-    public void updateUI(boolean isLogin){
-        if(isLogin){
-            prof_section.setVisibility(View.VISIBLE);
-            SignIn.setVisibility(View.GONE);
-        } else {
-            prof_section.setVisibility(View.GONE);
-            SignIn.setVisibility(View.VISIBLE);
-        }
-    }
+
 
     public void signOut(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
         Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
             @Override
             public void onResult(@NonNull Status status) {
+
+                progressDialog.setMessage("Removing Google+ Cache...");
+                progressDialog.show();
+
                 updateUI(false);
+
+                progressDialog.dismiss();
             }
         });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly. We can try and retrieve an
+            // authentication code.
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Fetching Google+ Sign in...");
+            progressDialog.show();
+            GoogleSignInResult result = opr.get();
+            handleResult(result);
+            updateUI(true);
+            progressDialog.dismiss();
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Checking sign in state...");
+            progressDialog.show();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    progressDialog.dismiss();
+                    handleResult(googleSignInResult);
+                    updateUI(false);
+                }
+            });
+        }
+    }
+
+    public void updateUI(boolean isLogin){
+        if(isLogin){
+            prof_section.setVisibility(View.VISIBLE);
+            goHome.setVisibility(View.VISIBLE);
+        } else {
+            prof_section.setVisibility(View.GONE);
+            goHome.setVisibility(View.VISIBLE);
+        }
+    }
 }
